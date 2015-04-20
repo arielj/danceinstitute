2247bc48d3bbe7d0c681502735ba774c88b479b7
Ariel Juodziukynas <arieljuod@gmail.com>
2015-04-20 17:27:59 -0300

    More "Klass" fields

--- gui/klasses.py
+++ gui/klasses.py
@@ -26,13 +26,55 @@ class KlassForm(gtk.Frame):
   
   def get_form_fields(self):
     self.name_l = gtk.Label('Nombre')
-    self.name_e = gtk.Entry(100)
+    self.name_e = gtk.Entry(30)
     self.name_e.set_text(self.klass.name)
     
+    self.fee_l = gtk.Label('Precio completo')
+    self.fee_e = gtk.Entry(5)
+    self.fee_e.set_text(self.klass.fee)
+    
+    self.half_fee_l = gtk.Label('Precio mitad')
+    self.half_fee_e = gtk.Entry(5)
+    self.half_fee_e.set_text(self.klass.half_fee)
+    
+    self.once_fee_l = gtk.Label('Precio por clase')
+    self.once_fee_e = gtk.Entry(5)
+    self.once_fee_e.set_text(self.klass.once_fee)
+    
+    self.min_age_l = gtk.Label('Edad mínima')
+    self.min_age_e = gtk.Entry(2)
+    self.min_age_e.set_text(self.klass.min_age)
+    
+    self.max_age_l = gtk.Label('Edad máxima')
+    self.max_age_e = gtk.Entry(2)
+    self.max_age_e.set_text(self.klass.max_age)
+    
+    self.quota_l = gtk.Label('Cupo máximo')
+    self.quota_e = gtk.Entry(2)
+    self.quota_e.set_text(self.klass.quota)
+    
+    self.info_l = gtk.Label('Información')
+    self.info_e = gtk.TextView()
+    self.info_e.set_editable(True)
+    self.info_e.set_text(self.klass.info)
+    
     fields = gtk.VBox()
-    fields.pack_start(self.name_l,False)
-    fields.pack_start(self.name_e,False)
-      
+    fields.pack_start(self.name_l, False)
+    fields.pack_start(self.name_e, False)
+    fields.pack_start(self.fee_l, False)
+    fields.pack_start(self.fee_e, False)
+    fields.pack_start(self.half_fee_l, False)
+    fields.pack_start(self.half_fee_e, False)
+    fields.pack_start(self.once_fee_l, False)
+    fields.pack_start(self.once_fee_e, False)
+    fields.pack_start(self.min_age_l, False)
+    fields.pack_start(self.min_age_e, False)
+    fields.pack_start(self.max_age_l, False)
+    fields.pack_start(self.max_age_e, False)
+    fields.pack_start(self.quota_l, False)
+    fields.pack_start(self.quota_e, False)
+    fields.pack_start(self.info_e, True, False)
+
     return fields
   
   def get_values(self):
--- models/klass.py
+++ models/klass.py
@@ -5,9 +5,13 @@ class Klass():
   def __init__(self):
     self.id = False
     self.name = ''
-    self.from_time = '16:00'
-    self.to_time = '17:00'
-    self.room = 'Agua'
+    self.fee = '0'
+    self.half_fee = '0'
+    self.once_fee = '0'
+    self.min_age = '0'
+    self.max_age = '0'
+    self.quota = '0'
+    self.info = ''
     self.teachers = []
     self.schedules = []
     self.users = []
@@ -17,6 +21,13 @@ class Klass():
     klass = cls()
     klass.id = id
     klass.name = 'Flamenco Adultos'
+    klass.fee = '350'
+    klass.half_fee = '200'
+    klass.once_fee = '50'
+    klass.min_age = '15'
+    klass.max_age = '0'
+    klass.quota = '15'
+    klass.info = 'Traer zapatos con taco ancho y una pollera larga.'
     klass.teachers = [Teacher.find(1)]
     klass.schedules = [Schedule({'from_time': '20:30', 'to_time': '21:30', 'room': 'Fuego', 'day': 0}),
                        Schedule({'from_time': '20:30', 'to_time': '21:30', 'room': 'Fuego', 'day': 3})]
